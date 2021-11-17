c405c237e0bc9f55124529131cdd6a075fe98cdf
Joseph <osekuemen@gmail.com>
2021-11-17 19:56:06 +0100

    full screen mode activated

--- app/src/main/java/com/josephagu/minipaint/MainActivity.kt
+++ app/src/main/java/com/josephagu/minipaint/MainActivity.kt
@@ -2,10 +2,20 @@ package com.josephagu.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Create an instance of MyCanvasView.
+        val myCanvasView = MyCanvasView(this)
+
+     // request the full screen for the layout of myCanvasView by setting the SYSTEM_UI_FLAG_FULLSCREEN
+        // flag on myCanvasView. In this way, the view completely fills the screen.
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        // add a content description
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
     }
 }
---
+++ app/src/main/java/com/josephagu/minipaint/MyCanvasView.kt
@@ -0,0 +1,12 @@
+package com.josephagu.minipaint
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+
+class MyCanvasView (context: Context) : View(context) {
+
+ 
+}
